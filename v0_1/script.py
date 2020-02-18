b0761b92cc9aca6d991f71cb219bf53ec089c5e8
Oz Locke <oz@itsalocke.com>
2020-02-18 15:51:54 +0000

    changed system_messages variable to be called verbose

--- v0_1/script.py
+++ v0_1/script.py
@@ -3,9 +3,9 @@
 import csv, os
 
 class UserInput:
-    def __init__(self, pantry, system_messages = False):
+    def __init__(self, pantry, verbose = False):
         self.pantry_instance = pantry
-        self.system_messages = system_messages
+        self.verbose = verbose
     
     def __repr__(self):
         return "an instance of the UserInput functional class"
@@ -32,11 +32,11 @@ class UserInput:
             quantity = input("\nWhat quantity of this item should I store?\n\n")
             
             # Add to pantry list
-            self.pantry_instance.update_pantry(item, unit, quantity, self.system_messages)
+            self.pantry_instance.update_pantry(item, unit, quantity, self.verbose)
             # Check for continue
             more = input("\nWould you like to add another item? (y or n)\n\n")
         # Write to database
-        self.pantry_instance.write_pantry(self.system_messages)
+        self.pantry_instance.write_pantry(self.verbose)
 
     def user_input(self):
         user_choice = int(input("What would you like to do?\n1 - Review Pantry Contents\n2 - View available units\n3 - Update Pantry\n4 - Quit\n\n"))
@@ -78,47 +78,47 @@ class Pantry:
                 pantry_item.update(row)
                 # And then append the dict to the pantry list
                 self.pantry.append(pantry_item)
-            print("Finished reading file\n---------------------")
+            print("SYSTEM: Finished reading file\n---------------------")
 
     def __repr__(self):
-        return f"an instance of the Pantry functional class called {self.pantry_name}"
+        return f"\nan instance of the Pantry functional class called {self.pantry_name}"
 
     def display_pantry(self):       
         for item in self.pantry:
             print("{item} | {unit} | {quantity}".format(item=item["item"], unit=item["unit"], quantity=item["quantity"]))
 
-    def update_pantry(self, item, unit, quantity, system_massages = False):
-        if system_massages: print("SYSTEM: Checking item against pantry")
+    def update_pantry(self, item, unit, quantity, verbose):
+        if verbose: print("SYSTEM: Checking item against pantry")
         if any(entry["item"] == item for entry in self.pantry):
             # Get the index of the item so we can access the dictionary within the pantry list by index
             item_index = next((index for (index, entry) in enumerate(self.pantry) if entry["item"] == item), None)
             if self.pantry[item_index]["unit"] == unit:
-                if system_massages: print(f"SYSTEM: Adding {quantity} of {item} to existing item")
+                if verbose: print(f"SYSTEM: Adding {quantity} of {item} to existing item")
                 pantry_quantity = int(self.pantry[item_index]["quantity"])
                 pantry_quantity += int(quantity)
                 self.pantry[item_index]["quantity"] = str(pantry_quantity)
             else:
                 # Build dictionary and add to pantry list
-                if system_massages: print(f"SYSTEM: Creating new entry for duplicate {item} with new unit of {unit} and quantity of {quantity}")
+                if verbose: print(f"SYSTEM: Creating new entry for duplicate {item} with new unit of {unit} and quantity of {quantity}")
                 new_entry = {"item": item, "unit": unit, "quantity": quantity}
                 self.pantry.append(new_entry)
         else:
             # Build dictionary and add to pantry list
-            if system_massages: print(f"SYSTEM: Creating new entry for {item} with uinit of {unit} and quantity of {quantity}")
+            if verbose: print(f"SYSTEM: Creating new entry for {item} with uinit of {unit} and quantity of {quantity}")
             new_entry = {"item": item, "unit": unit, "quantity": quantity}
             self.pantry.append(new_entry)
-        if system_massages: print("Finished updating pantry\n---------------------")
+        if verbose: print("SYSTEM: Finished updating pantry\n---------------------")
     
-    def write_pantry(self, system_massages = False):
+    def write_pantry(self, verbose):
         # Overwrite CSV
         with open(self.data_file, "w", newline="") as pantry_csv:
-            if system_massages: print(f"SYSTEM: Writing to file {self.file_name}")
+            if verbose: print(f"SYSTEM: Writing to file {self.file_name}")
             headers = ["item", "unit", "quantity"]
             writer = csv.DictWriter(pantry_csv, fieldnames=headers)
             writer.writeheader()
             for item in self.pantry:
                 writer.writerow(item)
-        if system_massages: print("Finished writing to file\n---------------------")
+        if verbose: print("SYSTEM: Finished writing to file\n---------------------")
 
 
 
