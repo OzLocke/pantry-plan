341980627e41d9b5341b6b7989892ce39ee1c00e
Oz Locke <oz@itsalocke.com>
2020-02-19 18:01:24 +0000

    Added area column and associated functionality

--- v0_1/script.py
+++ v0_1/script.py
@@ -10,10 +10,13 @@ class UserInput:
     def __repr__(self):
         return "an instance of the UserInput functional class"
 
-    def display_units(self):
-        # Create nicely written list of units
-        units = ', '.join(self.pantry_instance.unit_types)
-        print(f"\nThe available units are: {units}.\n---------------------\n")
+    def display_types(self, d_type):
+        d_type_lookup = {"unit": self.pantry_instance.unit_types, "area": self.pantry_instance.area_types}
+        d_type = d_type_lookup[d_type]
+        # Create nicely written list
+        types = ', '.join(d_type)
+        print(f"\nThe available units are: {types}.\n---------------------\n")
+        
 
     def input_pantry_entries(self):
         more = "y"
@@ -26,13 +29,22 @@ class UserInput:
                 unit = input("\nWhat unit should I store the item as?\n\n")
                 if not unit in self.pantry_instance.unit_types:
                     print(f"Sorry, {unit} is not a valid unit.")
-                    self.display_units()
+                    self.display_types("unit")
                 else:
                     unit_error = False
             quantity = input("\nWhat quantity of this item should I store?\n\n")
-            
+            # Ensure unit input is valid
+            area_error = True
+            while area_error:
+                area = input("\nAnd where will you be storing it?\n\n")
+                if not area in self.pantry_instance.area_types:
+                    print(f"Sorry, {area} is not a valid area.")
+                    self.display_types("area")
+                else:
+                    area_error = False
+
             # Add to pantry list
-            self.pantry_instance.update_pantry(item, unit, quantity, self.verbose)
+            self.pantry_instance.update_pantry(item, unit, quantity, area, self.verbose)
             # Check for continue
             more = input("\nWould you like to add another item? (y or n)\n\n")
         # Write to database
@@ -43,12 +55,12 @@ class UserInput:
 
         # Call methods from linked pantry instance based on user input
         if user_choice == 1:
-            print("\nOK, here' the current pantry list:\n\nItem | Unit | Quantity")
+            print("\nOK, here' the current pantry list:\n\nItem | Unit | Quantity | Area")
             self.pantry_instance.display_pantry()
             print("---------------------\n")
             self.user_input()
         if user_choice == 2:
-            self.display_units()
+            self.display_types("unit")
             self.user_input()
         if user_choice == 3:
             self.input_pantry_entries()
@@ -66,6 +78,7 @@ class Pantry:
         # set up variables
         self.pantry = []
         self.unit_types = ["bag", "box", "bottle", "carton", "tin", "case", "pack", "kg", "l"]
+        self.area_types = ["fridge", "freezer", "cupboard"]
         self.pantry_name = p_name
 
         # Load in existing pantry data
@@ -81,13 +94,13 @@ class Pantry:
             print("SYSTEM: Finished reading file\n---------------------")
 
     def __repr__(self):
-        return f"\nan instance of the Pantry functional class called {self.pantry_name}"
+        return f"\nAn instance of the Pantry functional class called {self.pantry_name}"
 
     def display_pantry(self):       
         for item in self.pantry:
-            print("{item} | {unit} | {quantity}".format(item=item["item"], unit=item["unit"], quantity=item["quantity"]))
+            print("{item} | {unit} | {quantity} | {area}".format(item=item["item"], unit=item["unit"], quantity=item["quantity"], area=item["area"]))
 
-    def update_pantry(self, item, unit, quantity, verbose):
+    def update_pantry(self, item, unit, quantity, area, verbose):
         if verbose: print("SYSTEM: Checking item against pantry")
         if any(entry["item"] == item for entry in self.pantry):
             # Get the index of the item so we can access the dictionary within the pantry list by index
@@ -99,13 +112,13 @@ class Pantry:
                 self.pantry[item_index]["quantity"] = str(pantry_quantity)
             else:
                 # Build dictionary and add to pantry list
-                if verbose: print(f"SYSTEM: Creating new entry for duplicate {item} with new unit of {unit} and quantity of {quantity}")
-                new_entry = {"item": item, "unit": unit, "quantity": quantity}
+                if verbose: print(f"SYSTEM: Creating new entry for duplicate {item} with new unit of {unit} and quantity of {quantity} in the {area}")
+                new_entry = {"item": item, "unit": unit, "quantity": quantity, "area": area}
                 self.pantry.append(new_entry)
         else:
             # Build dictionary and add to pantry list
-            if verbose: print(f"SYSTEM: Creating new entry for {item} with uinit of {unit} and quantity of {quantity}")
-            new_entry = {"item": item, "unit": unit, "quantity": quantity}
+            if verbose: print(f"SYSTEM: Creating new entry for {item} with uinit of {unit} and quantity of {quantity} in the {area}")
+            new_entry = {"item": item, "unit": unit, "quantity": quantity, "area": area}
             self.pantry.append(new_entry)
         if verbose: print("SYSTEM: Finished updating pantry\n---------------------")
     
@@ -113,7 +126,7 @@ class Pantry:
         # Overwrite CSV
         with open(self.data_file, "w", newline="") as pantry_csv:
             if verbose: print(f"SYSTEM: Writing to file {self.file_name}")
-            headers = ["item", "unit", "quantity"]
+            headers = ["item", "unit", "quantity", "area"]
             writer = csv.DictWriter(pantry_csv, fieldnames=headers)
             writer.writeheader()
             for item in self.pantry:
