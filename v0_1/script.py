057d0c61584b5b1dcf4ec1f5d9e8602006dbdd21
Oz Locke <oz@itsalocke.com>
2020-02-20 11:52:59 +0000

    Finished adding error checking

--- v0_1/script.py
+++ v0_1/script.py
@@ -18,27 +18,31 @@ class UserInput:
         print(f"\nThe available {d_type}s are: {types}.\n---------------------\n")
         
     def input_pantry_entries(self):
-        # Create a function that allows checking user input where needed
-        def type_error_check(i_type):
-            # Ensure unit input is valid
-            type_error = True
-            texts = {"unit": "What unit should I store the item as?", "area": "What area should I store the item in?"}
-            while type_error:
-                u_input = input(f"\n{texts[i_type]}\n\n")
-                if not u_input in self.type_lookup[i_type]:
-                    print(f"\nSorry, that's not a valid input.")
-                    self.display_types(i_type)
-                else:
-                    type_error = False
-            return u_input
-        
+        # Create instance of error checker that can be used by the function
+        self.error_checker = ErrorChecker()
+        # Run input with a loop so user can add more than one item
         more = "y"
         while more == "y":
             # Take input for each dictionary value
             item = input("\nWhat is the item called?\n\n")
-            unit = type_error_check("unit")
-            quantity = input("\nWhat quantity of this item should I store?\n\n")
-            area = type_error_check("area")
+            
+            error = True
+            while error:
+                unit = input("\nWhat unit should I store the item as?\n\n")
+                error = self.error_checker.type_error_check(unit, "unit")
+            
+            error = True
+            while error:
+                try:
+                    quantity = int(input("\nWhat quantity of this item should I store?\n\n"))
+                    error = False
+                except:
+                    print(f"\nSorry, that's not a valid input.")
+
+            error = True
+            while error:
+                area = input("\nWhat area should I store the item in?\n\n")
+                error = self.error_checker.type_error_check(area, "area")
 
             # Add to pantry list
             self.pantry_instance.update_pantry(item, unit, quantity, area, self.verbose)
@@ -48,26 +52,47 @@ class UserInput:
         self.pantry_instance.write_pantry(self.verbose)
 
     def user_input(self):
-        user_choice = int(input("What would you like to do?\n1 - Review Pantry Contents\n2 - View available units\n3 - View available areas\n4 - Update Pantry\n5 - Quit\n\n"))
+        error = True
+        while error:
+            try:
+                user_choice = int(input("What would you like to do?\n1 - Review Pantry Contents\n2 - View available units\n3 - View available areas\n4 - Update Pantry\n5 - Quit\n\n"))
 
-        # Call methods from linked pantry instance based on user input
-        if user_choice == 1:
-            print("\nOK, here' the current pantry list:\n\nItem | Unit | Quantity | Area")
-            self.pantry_instance.display_pantry()
-            print("---------------------\n")
-            self.user_input()
-        if user_choice == 2:
-            self.display_types("unit")
-            self.user_input()
-        if user_choice == 3:
-            self.display_types("area")
-            self.user_input()
-        if user_choice == 4:
-            self.input_pantry_entries()
-            self.user_input()
-        if user_choice == 5:
-            print("\nGoodbye!")
+                # Call methods from linked pantry instance based on user input
+                if user_choice == 1:
+                    print("\nOK, here' the current pantry list:\n\nItem | Unit | Quantity | Area")
+                    self.pantry_instance.display_pantry()
+                    print("---------------------\n")
+                    self.user_input()
+                elif user_choice == 2:
+                    self.display_types("unit")
+                    self.user_input()
+                elif user_choice == 3:
+                    self.display_types("area")
+                    self.user_input()
+                elif user_choice == 4:
+                    self.input_pantry_entries()
+                    self.user_input()
+                elif user_choice == 5:
+                    print("\nGoodbye!")
+                else:
+                    raise ValueError
 
+                error = False
+            except:
+                print(f"\nSorry, that's not a valid input.\n")
+
+class ErrorChecker(UserInput):
+    def __init__(self):
+        # Link to UserInput class to be able to access it's attributes
+        UserInput.__init__(self, verbose = False)
+
+    def type_error_check(self, u_input, i_type):
+        if not u_input in self.type_lookup[i_type]:
+            print(f"\nSorry, that's not a valid input.")
+            self.display_types(i_type)
+            return True 
+        else:
+            return False
 class Pantry:
     def __init__(self):
         # set up directory
